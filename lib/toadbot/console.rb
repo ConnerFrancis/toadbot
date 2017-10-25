7eaa4c33cfa71ce48fff44a58fa85371c919a482
kumo <thxbro79@gmail.com>
2017-10-25 18:51:41 +0000

    Console work 3, threelectric boogathroo.

--- lib/toadbot/console.rb
+++ lib/toadbot/console.rb
@@ -3,8 +3,21 @@ module Toadbot
   # A console for the bot
   # Used to manage stuff
   module Console
+    # Just do it.
+    extend self
+    
     # Give a little bit o' space.
     puts
+    
+    # Start the config
+    DEFAULTS = {
+      
+      prefix:     '>',
+      watch_mode: 'sync'
+      
+    }.freeze
+    
+    @config = DEFAULTS
     
     # List of commands
     attr_reader :list
@@ -18,23 +31,23 @@ module Toadbot
     # Used to define new commands.
     class Command
       # You can get the name and attribute of the command.
-      attr_reader :name, :arguments, :attributes
+      attr_reader :name, :arguments, :attrs
       
       # &block gets the block of code passed to the
       # method, see "test" example below
-      def initialize(name, arguments={}, attributes = {}, block)
+      def initialize(name, arguments = {}, attrs = {}, block)
         # Name of command
         @name = name
         
         # Arguments passed to command
-        # A hash of arguments
+        # A hash of arguments2
         @arguments = arguments
         
         # Attributes to command
         # A hash of attributes
-        @attributes = {
-          # atr_name: attributes[:atr_name] || default_value
-          test: attributes[:test] || nil
+        @attrs = {
+          # Description tag
+          desc: attrs[:desc] || 'No description provided.'
         }
         
         # To call the block simply do:
@@ -76,68 +89,33 @@ module Toadbot
     # Define the console command method
     # This is essentially shorthand syntax
     # You can extend Console module for access or use Console::command
-    def self.command(name, arguments = [], attributes = {}, &block)
+    def command(name, arguments = [], attributes = {}, &block)
       # Enter the command into the command
       # list hash with the name of the command.
       @list[name] = Command.new(name, arguments, attributes, block)
     end
     
-    # | | operators get block.call() arguments
-    command(:say, {string: nil, color: nil}) do | args |
-      puts args[:string]
-    end
-    
-    command(:exit) do
-      exit
-    end
-    
-    # command(:reload, [:module]) do
-    #   begin
-    #     Toadbot.inload(option)
-    #   rescue
-    #     puts option.red.underline + ' is not a module available for reload!'.red
-    #   end
-    # end
-    
-    # command(:irb) do
-    #   puts
-    #   puts 'Eval:'.bold
-    #   # Get option and eval
-    #   begin
-    #     option = gets.chomp
-    #     eval option
-    #     puts 'Eval successful'.bold
-    #     puts
-    #   # If fail, tell an error happened
-    #   rescue
-    #     puts 'Error.'.bold
-    #     puts
-    #   end
-    # end
-    
-    # This loop always runs and constantly
-    # gets the option from the user.
-    loop do
+    private def console
       # This is what the console runs on.
       # This creates a constant gets form.
       #
       # i.e.:
       # say hello it me
-      print '> '
-      command = gets.chomp
+      print "#{@config[:prefix]} "
+      string = gets.chomp
       
       # Split the words into individual values
       # in an array
       #
       # i.e.:
       # {'say', 'hello', 'it', 'me'}
-      command = command.split
+      string = string.split
       # They command keyword is the first
       # value of the command array
-      keyword = command.first.to_sym
+      keyword = string.first.to_sym
       # Shift command so it removes the first portion
       # This only returns args and not the command word
-      command.shift
+      string.shift
       
       # Call the command. If it isn't found
       # give console error.
@@ -145,17 +123,71 @@ module Toadbot
         # Simply call the command.
         # Pass the arguments, or shifted command, as
         # the given_args to the call.
-        @list[keyword].call(command)
+        @list[keyword].call(string)
       rescue
-        # Tell user the command doesn't exist
-        puts "Unknown command '#{keyword}'."
+        # Case when it doesn't exist
+        unless @list[keyword]
+          puts "Unknown command '#{keyword}'."
+          return
+        end
+        # Otherwise b rly vague
+        puts 'Error:'
+        puts $!
       end
     end
+    public
+    
+    # | | operators get block.call() arguments
+    command(:say, {string: nil, color: nil}) do | args |
+      puts args[:string]
+    end
+    
+    command(:reload, {mod: 'config'}, {desc: 'Reloads a module. reload `module`'}) do | mod |
+      # This looks ugly because it is
+      # We only want the value given, and
+      # not the key. This is how we do it
+      # though it may be ugry.
+      mod = mod[:mod]
+      
+      begin
+        Toadbot.inload(mod)
+      rescue
+        puts mod.underline + ' is not a module available for reload!'.red
+      end
+    end
+    
+    command(:irb, {option: nil}) do | s |
+      
+    end
+    
+    command(:help) do
+      puts 'Commands:'
+      @list.each do | key, c |
+        puts " | #{key} - #{c.attrs[:desc]}"
+      end
+    end
+    
+    command(:exit) do
+      exit
+    end
+    
+    # This loop always runs and constantly
+    # gets the option from the user.
+    def start 
+      case @config[:watch_mode]
+        when 'sync'
+          loop {console}
+        when 'async'
+          console
+        else
+          raise "@config[:watch_mode] cannot be '#{@config[:watch_mode]}'!"
+      end
+    end
+    
     # end of loop
-    
-    # End of the console code.
-    
   end
   # end of Console
+  
+  Console.start
 end
 # end of Toadbot
